bb525b87c5b382e78e82f84fb8ef592331117784
StrangePerch <sasha.aav.sv@gmail.com>
2021-06-10 20:13:35 +0300

    Replace " " on "_"

--- code.js
+++ code.js
@@ -8,6 +8,7 @@ function OnTextChanged() {
 }
 
 function Generate(id, in_n, out_n) {
+    id = id.replace(" ", "_");
     let markup = `<div id="${id}_container">`;
     markup += `<label id="${id}_label">`;
     
